255ea90debe96baca6d6a422f0b0faf79e97adb9
nnaaddiia <rnadia17025@gmail.com>
2025-03-22 00:19:36 +0700

    HomePage(UPDATE-2)

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,5 +1,7 @@
 import Homepage from "./pages/Homepage.tsx";
-import Welcome from "./pages/Welcome.tsx";
+//import Welcome from "./pages/Welcome.tsx";
+//import Calendar from "./pages/Calendar.tsx";
+
 
 function App() {
   return <Homepage/>;
